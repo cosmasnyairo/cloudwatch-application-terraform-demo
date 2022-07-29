8c41d3450602268217903ba5989004677d6cf7de
Cosmas Nyairo <nyairocosmas@gmail.com>
2022-07-30 00:29:49 +0300

    add sns, alarms, log streams and log groups

---
+++ data.tf
@@ -0,0 +1,28 @@
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__default_policy_ID"
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+      values   = [var.account-id]
+    }
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [aws_sns_topic.aws_event_notifications.arn]
+    sid       = "__default_statement_ID"
+  }
+}
--- resources.tf
+++ resources.tf
@@ -1,25 +1,41 @@
-# resource "aws_sns_topic" "aws_event_notifications" {
-#   name         = var.sns_topic_name
-#   display_name = var.dispay_name
-#   tags = merge(
-#     var.tags,
-#     {
-#       Name = var.sns_topic_name
-#     },
-#   )
-# }
+resource "aws_sns_topic" "aws_event_notifications" {
+  name         = var.sns_topic_name
+  display_name = var.dispay_name
+  tags = merge(
+    var.tags,
+    {
+      Name = var.sns_topic_name
+    },
+  )
+}
 
-# resource "aws_sns_topic_policy" "aws_event_notifications_policy" {
-#   arn    = aws_sns_topic.aws_event_notifications.arn
-#   policy = data.aws_iam_policy_document.sns_topic_policy.json
-# }
+resource "aws_sns_topic_policy" "aws_event_notifications_policy" {
+  arn    = aws_sns_topic.aws_event_notifications.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+resource "aws_cloudwatch_log_group" "demogroup" {
+  name = var.log_group_name
+  retention_in_days = 1
+  tags = merge(
+    var.tags,
+    {
+      Name = var.log_group_name
+    },
+  )
+}
+
+resource "aws_cloudwatch_log_stream" "demologstream" {
+  name           = var.log_stream_name
+  log_group_name = aws_cloudwatch_log_group.demogroup.name
+}
+
 
 resource "aws_cloudwatch_log_metric_filter" "MetricFilter" {
   for_each       = var.alarm_configs
   name           = each.value.metric_filter_name
-  log_group_name = var.log_group_name
+  log_group_name = aws_cloudwatch_log_group.demogroup.name
   pattern        = each.value.log_filter_pattern
-
   metric_transformation {
     name      = each.value.metric_name
     value     = each.value.metric_value
@@ -38,6 +54,7 @@ resource "aws_cloudwatch_dashboard" "main" {
   "widgets": [
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -49,7 +66,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -62,6 +79,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -73,7 +91,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -86,6 +104,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -97,7 +116,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -110,6 +129,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -121,7 +141,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -134,6 +154,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -143,7 +164,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -156,6 +177,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -172,7 +194,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -185,6 +207,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -196,7 +219,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -209,6 +232,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -225,7 +249,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -238,6 +262,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+   "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -254,7 +279,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -267,6 +292,7 @@ resource "aws_cloudwatch_dashboard" "main" {
     },
     {
       "type": "metric",
+      "liveData": true,
 
       "width": 6,
       "height": 8,
@@ -278,7 +304,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "view": "gauge",
         "stacked": false,
         "stat": "SampleCount",
-        "period": 30,
+        "period": 86400,
         "yAxis": {
           "left": {
             "min": 5,
@@ -295,44 +321,42 @@ resource "aws_cloudwatch_dashboard" "main" {
 EOF
 }
 
-# resource "aws_cloudwatch_metric_alarm" "CloudWatchAlarm" {
-#   for_each            = var.alarm_configs
-#   alarm_name          = each.value.alarm_name
-#   alarm_description   = each.value.alarm_description
-#   metric_name         = each.value.metric_name
-#   namespace           = each.value.metric_namespace
-#   statistic           = each.value.metric_statistic
-#   period              = each.value.metric_period
-#   threshold           = each.value.metric_threshold
-#   evaluation_periods  = each.value.metric_evaluation_periods
-#   comparison_operator = each.value.metric_comparison_operator
-#   treat_missing_data  = each.value.metric_treat_missing_data
-#   dimensions = {
-#       "$.functioncalled" = "$.functioncalled"
-#   }
-#   tags = merge(
-#     var.tags,
-#     {
-#       Name = each.value.alarm_name
-#     },
-#   )
-# }
+resource "aws_cloudwatch_metric_alarm" "CloudWatchAlarm" {
+  for_each            = var.alarm_configs
+  alarm_name          = each.value.alarm_name
+  alarm_description   = each.value.alarm_description
+  metric_name         = each.value.metric_name
+  namespace           = each.value.metric_namespace
+  statistic           = each.value.metric_statistic
+  period              = each.value.metric_period
+  threshold           = each.value.metric_threshold
+  evaluation_periods  = each.value.metric_evaluation_periods
+  comparison_operator = each.value.metric_comparison_operator
+  treat_missing_data  = each.value.metric_treat_missing_data
+  dimensions = {
+      "$.functioncalled" = "$.functioncalled"
+  }
+  tags = merge(
+    var.tags,
+    {
+      Name = each.value.alarm_name
+    },
+  )
+}
 
-
-
-# resource "aws_cloudwatch_composite_alarm" "CompositeAlarm" {
+resource "aws_cloudwatch_composite_alarm" "CompositeAlarm" {
   
-#   alarm_description = var.composite_alarm_description
-#   alarm_name        = var.composite_alarm_name
-#   alarm_actions     = [aws_sns_topic.aws_event_notifications.arn]
-#   alarm_rule        = var.alarm_rules               
-#   tags = merge(
-#     var.tags,
-#     {
-#       Name = var.composite_alarm_name
-#     },
-#   )
-#   depends_on = [
-#     aws_cloudwatch_metric_alarm.CloudWatchAlarm
-#   ]
-# }
+  alarm_description = var.composite_alarm_description
+  alarm_name        = var.composite_alarm_name
+  alarm_actions     = [aws_sns_topic.aws_event_notifications.arn]
+  alarm_rule        = var.alarm_rules               
+  tags = merge(
+    var.tags,
+    {
+      Name = var.composite_alarm_name
+    },
+  )
+  depends_on = [
+    aws_cloudwatch_metric_alarm.CloudWatchAlarm
+  ]
+}
